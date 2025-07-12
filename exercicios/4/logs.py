2a10455b21562725cdfc9ef7c814af99c98d8827
Davi Patricio <davipatricio2006@gmail.com>
2025-07-12 11:32:03 -0300

    fix formatting

--- exercicios/4/logs.py
+++ exercicios/4/logs.py
@@ -7,10 +7,10 @@ os_data = ""
 
 if sys.platform == "win32":
   win_ver = platform.win32_ver()[0]
-  os_data = "Windows {win_ver}"
+  os_data = f"Windows {win_ver}"
 elif sys.platform == "darwin":
   macos_ver = platform.mac_ver()
-  os_data = "MacOS {macos_ver}"
+  os_data = f"MacOS {macos_ver}"
 elif sys.platform == "linux":
   os_data = "Linux"
 
@@ -20,5 +20,8 @@ env_names = ", ".join(list(dict(os.environ).keys()))
 # Loaded modules
 module_names = ", ".join([mod for mod in sys.modules])
 
+# Python version
+py_version = sys.version
+
 with open(file="./ambient-info.txt", mode="w", encoding="utf8") as file:
-  file.write(f"Informações do sistema:\n\nSistema Operacional: {os_data}\nNome das Váriaveis de Ambiente: {env_names}\nMódulos carregados: {module_names}")
+  file.write(f"Informações do sistema:\n\nVersão do Python: {py_version}\nSistema Operacional: {os_data}\nNome das Váriaveis de Ambiente: {env_names}\nMódulos carregados: {module_names}")
